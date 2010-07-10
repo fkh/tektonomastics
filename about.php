55337f54b33d53dfc47a2ea406f2cf5a1d33693c
fkh <fkh@209921dc-6a5d-4c71-8264-c3f0a7d184bf>
2010-07-10 17:48:42 +0000

    About page, for #48
    git-svn-id: https://svn.tektonomastics.org/trunk@27 209921dc-6a5d-4c71-8264-c3f0a7d184bf

---
+++ about.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+	<title>Tektonomastics</title>
+	<?php include "include/head.inc"; ?>
+</head>
+
+<body onload="load()">
+	
+	<div id="body_container">
+		
+	<!-- header/navigation -->
+	<?php include "include/header.inc"; ?>
+	
+	
+
+ 	<h3>What is Tektonomastics?</h3>
+						<p>&#8220;Tektonomastics&#8221; is a word that we made up by combining &#8220;tekto-&#8221; (Greek for &#8220;building) with &#8220;<a title="International Council of Onomastic Sciences (ICOS)" target="_blank" href="http://www.icosweb.net/index.php/whatis-onomastics.html">onomastics</a>&#8221; (the study of the history and origin of proper names.  More established branches of onomatology include <a title="toponomastics" target="_blank" href="http://en.wikipedia.org/wiki/Toponymy">toponomastics</a> (the study of place names) and <a title="anthroponomastics" target="_blank" href="http://en.wikipedia.org/wiki/Anthroponymy">anthroponomastics</a> (the study of personal names).  We are creating a new branch - tektonomastics - or, the study of building names.</p>
+
+	<p>We first started noticing intriguing names on residential buildings around where we live near Prospect Park in Brooklyn.  A lot of these names refer to famous people (like <a href="http://tektonomastics.org/name/Martha_Washington" target="_blank">Martha Washington</a>, <a href="http://tektonomastics.org/name/Woodrow_Wilson" target="_blank">Woodrow Wilson</a>, <a href="http://tektonomastics.org/name/Abraham_Lincoln" target="_blank">Abraham Lincoln</a>), while others are typical &#8220;nice place&#8221; names (like <a href="http://tektonomastics.org/name/Majestic_Court" target="_blank">Majestic Court</a>, <a href="http://tektonomastics.org/name/Park_Central" target="_blank">Park Central</a>, <a href="http://tektonomastics.org/name/The_Lakeview" target="_blank">The Lakeview</a>).  But some building names are more enigmatic and poetic (<a href="http://tektonomastics.org/name/Ada" target="_blank">Ada</a>, <a href="http://tektonomastics.org/name/Launcelot" target="_blank">Launcelot</a>, <a href="http://tektonomastics.org/name/Lillianette" target="_blank">Lillianette</a>), making you wonder why someone chose that name for a building.  And some names are just downright quirky (<a href="http://tektonomastics.org/name/Mattowacks" target="_blank">Mattowacks</a>).  We began finding named buildings everywhere - in Astoria, Inwood Heights, Chelsea, and more - and realized just how many named buildings exist in New York City.</p>
+
+	<p>So we began wondering, why don&#8217;t we call these buildings by their proper names anymore?  Was there a time when friends said, &#8220;Let&#8217;s meet at the <a href="http://tektonomastics.org/name/Martinique" target="_blank">Martinique</a> at 6&#8221;? Or lovers wrote letters addressed to their sweethears at <a href="http://tektonomastics.org/name/Monbijou" target="_blank">Monbijou</a> on East 17th Street?</p>
+	<p>These eclectic names evoke a bygone era when owners, builders and designers named their buildings with personality (and humor).  By taking photographs and mapping the locations of these buildings, we hope to create a map of this forgotten landscape and help these neglected building names live once more.</p>
+	<p>Join us in our quest for building names!  <a title="Map" target="_blank" href="http://tektonomastics.org/map/">Add a building to the map</a>, <a title="Inventory" target="_blank" href="http://tektonomastics.org/inventory/">peruse the inventory</a>, or contribute notes about the buildings you know and love.</p>
+
+	<!-- footer -->
+	<?php include "include/footer.inc"; ?>
+	
+	</div>
+</body>
+</html>
