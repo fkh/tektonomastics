f4790ec9e92882969575973a403c0b2b9a46e22a
fkh <fkh@209921dc-6a5d-4c71-8264-c3f0a7d184bf>
2010-07-19 06:07:25 +0000

    fixes #63 (current "about" shows)
    git-svn-id: https://svn.tektonomastics.org/trunk@54 209921dc-6a5d-4c71-8264-c3f0a7d184bf

---
+++ contact.php
@@ -0,0 +1,25 @@
+
+<html>
+
+<head>
+	<title>Tektonomastics</title>
+	<?php include "include/head.inc"; ?>
+</head>
+
+<body onload="load()">
+	
+	<div id="body_container">
+		
+	<!-- header/navigation -->
+	<?php include "include/header.inc"; ?>
+	<?php include "include/navbar.inc"; ?>
+
+<iframe src="https://spreadsheets.google.com/embeddedform?formkey=dFc5MHpHUUJBd0lpSlZkcnNpejN5VFE6MQ" width="700" height="475" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+	<!-- footer -->
+	<?php include "include/footer.inc"; ?>
+	
+	</div>
+</body>
+</html>
+
