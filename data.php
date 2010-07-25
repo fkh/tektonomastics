3579d7f450124cc54c0a0e0fc6ea298f6f2aec5f
fkh <fkh@209921dc-6a5d-4c71-8264-c3f0a7d184bf>
2010-07-25 21:44:51 +0000

    Part of #30, kml export.
    git-svn-id: https://svn.tektonomastics.org/trunk@59 209921dc-6a5d-4c71-8264-c3f0a7d184bf

---
+++ data.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+	<title>Tektonomastics</title>
+	<?php include "include/head.inc"; ?>
+</head>
+
+<body onload="load()">
+	
+	<div id="body_container">
+		
+	<!-- header/navigation -->
+	<?php include "include/header.inc"; ?>
+	<?php include "include/navbar.inc"; ?>
+
+	<br><p>Thanks for sharing the building names and photos you spot on your travels! Grab locations of all the buildings here, to view them in Google Earth, GIS or another map program. 
+	
+	<p><a href="/makekml.php">Download all buildings in a Google Earth compatible KML file.</a></p>		
+		
+		<a href="http://blog.tektonomastics.org/">Let us know</a> if you make something cool with them.</p> <br>
+	
+	<!-- footer -->
+	<?php include "include/footer.inc"; ?>
+	
+	</div>
+</body>
+</html>
