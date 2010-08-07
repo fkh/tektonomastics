df57ddd937a735e74a92ca028f9fb96652e3ec48
fkh <fkh@209921dc-6a5d-4c71-8264-c3f0a7d184bf>
2010-08-07 21:48:49 +0000

    #8, link to the building after confirming - with functional db code.
    git-svn-id: https://svn.tektonomastics.org/trunk@65 209921dc-6a5d-4c71-8264-c3f0a7d184bf

--- verify.php
+++ verify.php
@@ -8,21 +8,24 @@
 			$building = releaseRecord($_GET['key']);
 			
 			if ($building > 0) {
-				
+			
+			$dbconnection = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error.');
+			mysql_select_db($dbname, $dbconnection);
+			
 			$query = "SELECT sortname FROM building WHERE id = '" . $building ."';";
 			
 			$db = mysql_query($query);
 
 			while ($row = mysql_fetch_array($db, MYSQL_BOTH)) {
 			
-			$confirmation = "Thanks! Your building is now included in the inventory.";
-			$confirmation .= "See it <a href='http://tektonomastics.org/map/";
-			$confirmation .= $row['sortname'];
-			$confirmation .= "'>here</a>.";
+				$confirmation = "Thanks! Your building is now included in the inventory." ;
+				$confirmation .= "See it <a href='http://tektonomastics.org/name/" ;
+				$confirmation .= $row['sortname'] ;
+				$confirmation .= "'>here</a>." ;
 			
 			}
-
-
+			
+			print $confirmation;
 
 			} else {
 
