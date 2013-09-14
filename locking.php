dd8a62ff194f4473a58a9265235d303b6ab21f90
fkh <frank@hebbert.com>
2013-09-14 13:29:59 -0400

    Email "from" needs to be a webfaction address

--- locking.php
+++ locking.php
@@ -36,6 +36,8 @@
 		//prep headers
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
 		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'Reply-to: tektonomastics@gmail.com' . "\r\n";
+		$headers .= 'From: tektonomastics@fkh.webfactional.com' . "\r\n";
 
 		//email to us
 		$devsubject = "New building! " . $name ;
