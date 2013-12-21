7d12f85021e2a5f7906e736bbb4a18f359cb122b
fkh <frank@hebbert.com>
2013-12-21 22:46:19 +0000

    adding table view of buildings

--- inventory.php
+++ inventory.php
@@ -3,21 +3,57 @@
 <head>
 	<title>Tektonomastics</title>
 	
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>  
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>  
+		<?php include "include/head.inc"; ?>
 	
+	<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>	 
+	-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>  
 
+  <!-- jQuery -->
+  <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+
+  <!-- DataTables -->
+  <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
 	
 	<script type="text/javascript"> 
+
+	// make the table interactive
+	$(document).ready(function() {
+      $('#buildings').dataTable({
+      "bPaginate": false,
+      "bLengthChange": false,
+      "bFilter": true,
+      "bSort": true,
+      "bInfo": false,
+      "bAutoWidth": true
+      });
+  } );
+	
 		function loadPhotos() {
+		  
 			$.get('/getphotos.php?recent=1', function(data){
 				$('#recent-pics').html(data);
 							});
-		}
+							
+				/** 			
+			var pics = $('.pic');
+      for (var i = 0; i < pics.length; ++i) {
+        
+        var pic = $('.pic').eq(i);
+        var currentBuilding = pic.text();
+	      var picUrl = "/getphotos.php?id=" + currentBuilding;
+		    $.get(picUrl, function(data){
+		      $('.pic').eq(i).html(data);
+  	    });
+        };	
+        
+        **/
+		};
+		
 	</script>
 	
 	
-	<?php include "include/head.inc"; ?>
+
 	  
 	</head>
 
@@ -58,46 +94,40 @@
 		echo "<div id='recent-pics'><em>Loading pics of recently-added buildings...</em></div>";
 			
 			echo "<p>There are " . $records . " buildings in the Inventory.</p>\n"; 
-			
-			$first = "Z";
+
 			echo "<div>";
+						
+			echo "<table class='dataTable' id='buildings'>";
+			echo "<thead>";
+			echo "<tr>";
+			echo "<th>Name</th>";
+			echo "<th>Address</th>";
+			echo "<th>City</th>";
+			echo "</tr>";
+			echo "</thead>\n";
+			
+
 			
 			while ($row = mysql_fetch_array($db, MYSQL_BOTH)) {								
 
 				$name = $row['SORTNAME']; // this has no "the"
 				$prettyname = stripslashes($name);
-				$quantity = $row['quantity'];
-				
-				$initial = substr($name, 0, 1);
-				if ( $initial !== $first) {
-					echo "</div><div class='invfloat'>"; 
-					echo "<h2>" . $initial . "</h2>" ;
-					$first = $initial ;
-				} 
-				
-				// $linkname = str_replace(" ", "_", $name) ;
 				$linkname = urlencode($prettyname) ;
 				
+				echo "<tr>";
+				echo "<td class='building-name'><a href='/name/" . $linkname . "'>" . $prettyname . "</a></td>";
+				echo "<td class='building-address'></td>";
+				echo "<td class='building-city'></td>";				
+				echo "</tr>\n";
 				
-				echo "<p><a href='/name/" . $linkname . "'>" . $prettyname . "</a>";
-				
-				if ($quantity > 1) {
-					echo " (" . $quantity . " buildings)" ;
-				}
-				
-				"</p>";
-			
-				// this works for the thumbnail pics		
-				//	echo "<div class='bldgfloat' id='build" . $row[0] . "'>" ;
-				//	echo $row[1]; 
-				//	echo "<br></div>";
-
 				}
 			
 				mysql_close($dbconnection);
 			
 			?>
 			
+			</table>
+		
 		</div>
 		
 		<div id='sidebar'>
